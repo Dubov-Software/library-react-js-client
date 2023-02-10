38cd67e820f7eb96e73b133d34419049bf4edff2
Alon Dubov <alon@alon.codes>
2023-02-10 01:51:42 +0100

    Adding image

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
+import { Box, Stack } from '@mui/system';
 
 const Header = () => {
     return (
-        <Grid container>
-            <Grid item xs={12}>
-                <Typography textAlign="center" variant="h5">Books Shelf</Typography>
-            </Grid>
-        </Grid>
+        <Stack spacing={1} columnGap={2} flexDirection="row" justifyContent="center" alignItems="center" justifyItems="center" alignContent="center">
+            <Box>
+                <img src="/books.png" height="75" />
+            </Box>
+            <Box>
+                <Typography textAlign="center" fontWeight="bold" color="secondary" variant="h5">Books</Typography>
+            </Box>
+        </Stack>
     );
 };
 
